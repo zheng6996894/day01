a02abc82c223de0588ce633e821672e8550dccce
zheng <1742161550@qq.com>
2021-09-03 16:45:36 +0800

    一个学生类 老师类里面新加了一个属性

--- src/com/zheng/entity/Teach.java
+++ src/com/zheng/entity/Teach.java
@@ -1,4 +1,20 @@
 package com.zheng.entity;
 
 public class Teach {
+    private String name;
+
+    public Teach() {
+    }
+
+    public Teach(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
